9d415ffb1988e0d6ec3e45ea27128e20dfa1b76c
pablocontrerasc <cont.pab@gmail.com>
2022-05-05 05:35:54 +0000

    codigo ok

--- src/main.py
+++ src/main.py
@@ -8,7 +8,7 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
+from models import db, User, Usuario, Personaje, Planeta, PersonajeFavoritos, PlanetaFavoritos
 #from models import Person
 
 app = Flask(__name__)
@@ -39,6 +39,111 @@ def handle_hello():
 
     return jsonify(response_body), 200
 
+@app.route('/users', methods=['GET'])
+def get_user():
+
+    usuario = Usuario.query.all()
+    response_body = list(map(lambda x: x.serialize(), usuario))
+ 
+    return jsonify(response_body), 200
+
+@app.route('/people', methods=['GET'])
+def get_people():
+    personaje = Personaje.query.all()
+    response_body = list(map(lambda x: x.serialize(), personaje))
+    return jsonify(response_body), 200
+
+@app.route('/people/<int:people_id>', methods=['GET'])
+def get_one_people(people_id):
+    personaje = Personaje.query.get(id = people_id)
+    response_body = list(map(lambda x: x.serialize(), personaje))
+    return jsonify(response_body), 200
+
+@app.route('/planet', methods=['GET'])
+def get_planet():
+    planeta = Planeta.query.all()
+    response_body = list(map(lambda x: x.serialize(), planeta))
+    return jsonify(response_body), 200
+
+@app.route('/planet/<int:planet_id>', methods=['GET'])
+def get_one_planet(planet_id):
+    planeta = Planeta.query.filter_by(id = planet_id)
+    response_body = list(map(lambda x: x.serialize(), planeta))
+    return jsonify(response_body), 200
+
+@app.route('/users/favorites', methods=['GET'])
+def get_users_favorites():
+    usuario_id= request.json.get("usuario_id")
+
+    personajes = PersonajeFavoritos.query.filter_by(usuario_id= usuario_id)
+    lista_personajes = list(map(lambda x: x.serialize(), personajes))
+
+    planetas = PlanetaFavoritos.query.filter_by(usuario_id= usuario_id)
+    lista_planetas = list(map(lambda x: x.serialize(), planetas))
+
+    response_body = {
+        "Personajes_favoritos": lista_personajes,
+        "Planetas_favoritos": lista_planetas
+    }
+
+    return  jsonify(response_body), 200
+
+@app.route("/users", methods=["POST"])
+def new_user():
+    try:
+        nombre = request.json.get("nombre", None)
+        apellido = request.json.get("apellido", None)
+        email = request.json.get("email", None)
+        password = request.json.get("password", None)
+        fecha_subscripcion = request.json.get("fecha_subscripcion", None)
+       
+        userR = Usuario.query.filter_by(email=email).first()
+        if userR:
+            return {"Error": "Correo ya registrado"}
+        user = Usuario(
+            nombre=nombre, apellido=apellido, email=email, password=password, fecha_subscripcion=fecha_subscripcion
+        )
+        db.session.add(user)
+        db.session.commit()
+        return {"mensaje": "ok"}, 200
+    except Exception as e:
+        print(f"new_user_ERROR: {e}")
+        return (f"new_user_ERROR: {e}"), 500
+
+
+@app.route('/favorite/planet/<int:planet_id>', methods=['POST'])
+def add_favorite_planet(planet_id): 
+    usuario_id = request.json.get("usuario_id")
+    planeta_favorito = PlanetaFavoritos(usuario_id, planeta_id=planet_id)
+    db.session.add(planeta_favorito)
+    db.session.commit()
+    return "Planeta favorito agregado", 200
+
+@app.route('/favorite/people/<int:people_id>', methods=['POST'])
+def add_favorite_peple(people_id): 
+    usuario_id = request.json.get("usuario_id")
+    personaje_favorito = PersonajeFavoritos(usuario_id, personaje_id=people_id)
+    db.session.add(personaje_favorito)
+    db.session.commit()
+    return "Personaje favorito agregado", 200
+
+
+@app.route('/favorite/planet/<int:planet_id>', methods=['DELETE'])
+def delete_favorite_planet(planet_id): 
+    usuario_id = request.json.get("usuario_id")
+    planeta_favorito = PlanetaFavoritos.query.filter_by(usuario_id = usuario_id, planeta_id=planet_id).first()
+    db.session.delete(planeta_favorito)
+    db.session.commit()
+    return "Planeta favorito eliminado", 200
+
+@app.route('/favorite/people/<int:people_id>', methods=['DELETE'])
+def delete_favorite_people(people_id): 
+    usuario_id = request.json.get("usuario_id")
+    personaje_favorito = PersonajeFavoritos.query.filter_by(usuario_id = usuario_id, personaje_id=people_id).first()
+    print(personaje_favorito,"planteFF")
+    db.session.delete(personaje_favorito)
+    db.session.commit()
+    return "Planeta favorito eliminado", 200
 # this only runs if `$ python src/main.py` is executed
 if __name__ == '__main__':
     PORT = int(os.environ.get('PORT', 3000))
--- src/models.py
+++ src/models.py
@@ -1,5 +1,6 @@
 from flask_sqlalchemy import SQLAlchemy
 
+
 db = SQLAlchemy()
 
 class User(db.Model):
@@ -16,4 +17,155 @@ class User(db.Model):
             "id": self.id,
             "email": self.email,
             # do not serialize the password, its a security breach
+        }
+
+class Usuario(db.Model):
+    __tablename__ = 'usuario'
+    id = db.Column(db.Integer, primary_key=True)
+    nombre = db.Column(db.String(100))
+    apellido = db.Column(db.String(100))
+    email = db.Column(db.String(150))
+    password = db.Column(db.String(150))
+    fecha_subscripcion = db.Column(db.String(100))
+    
+    def __init__(self,nombre,apellido,email, password,fecha_subscripcion):
+        self.nombre = nombre
+        self.apellido = apellido
+        self.email = email
+        self.password = password
+        self.fecha_subscripcion = fecha_subscripcion
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "nombre": self.nombre,
+            "apellido": self.apellido,
+            "email": self.email,
+            "fecha_subscripcion": self.fecha_subscripcion
+        }
+
+
+class Personaje(db.Model):
+    __tablename__ = 'personaje'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100))
+    height = db.Column(db.String(100))
+    mass = db.Column(db.String(100))
+    hair_color = db.Column(db.String(100))
+    skin_color = db.Column(db.String(100))
+    eye_color = db.Column(db.String(100))
+    birth_year = db.Column(db.String(100))
+    gender = db.Column(db.String(100))
+    homeworld = db.Column(db.String(150))
+    created = db.Column(db.String(100))
+
+    def __init__(self, id, name, height, mass, hair_color, skin_color, eye_color,birth_year,gender,homeworld,created):
+        self.id = id
+        self.name = name
+        self.height = height
+        self.mass = mass
+        self.hair_color = hair_color
+        self.skin_color = skin_color
+        self.eye_color = eye_color
+        self.birth_year = birth_year
+        self.gender = gender
+        self.homeworld = homeworld
+        self.created = created
+    
+    def __repr__(self):
+        return '<Personaje %r>' % self.name
+
+    def serialize(self):
+        return{
+            "id": self.id,
+            "name": self.name,
+            "heigth": self.height,
+            "mass": self.mass,
+            "hair_color": self.hair_color,
+            "skin_color": self.skin_color,
+            "eye_color": self.eye_color,
+            "birth_yeear": self.birth_year,
+            "gender": self.gender,
+            "homeworld": self.homeworld,
+            "creatd": self.created
+        }
+
+class Planeta(db.Model):
+    __tablename__ = 'planeta'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100))
+    orbital_period = db.Column(db.String(100))
+    rotation_period = db.Column(db.String(100))
+    diameter = db.Column(db.String(100))
+    climate = db.Column(db.String(100))
+    gravity = db.Column(db.String(100))
+    terrain = db.Column(db.String(100))
+    surface_water = db.Column(db.String(100))
+    population = db.Column(db.String(100))
+    created = db.Column(db.String(100))
+
+    def __init__(self, name, orbital_period, rotation_period, diameter, climate, gravity, terrain,surface_water, population, created):
+        self.name = name
+        self.orbital_period = orbital_period
+        self.rotation_period = rotation_period
+        self.diameter = diameter
+        self.climate = climate
+        self.gravity = gravity
+        self.surface_water = surface_water
+        self.population = population
+        self.created = created
+    
+    def __repr__(self):
+        return '<Planeta %r>' % self.name
+
+    def serialize(self):
+        return{
+            "id": self.id,
+            "name": self.name,
+            "orbital_period": self.orbital_period,
+            "rotation_period": self.rotation_period,
+            "diameter": self.diameter,
+            "climate": self.climate,
+            "gravity": self.gravity,
+            "surface_water":self.surface_water,
+            "papularion": self.population,
+            "created": self.created
+
+        }
+    
+class PlanetaFavoritos(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    __tablename__ = "planeta_favoritos"
+    usuario_id = db.Column(db.Integer, db.ForeignKey('usuario.id'))
+    planeta_id = db.Column(db.Integer, nullable=False)
+
+    def __init__(self, usuario_id, planeta_id):
+        self.usuario_id = usuario_id
+        self.planeta_id = planeta_id
+
+    def __repr__(self):
+        return '<PlanetaFavoritos %r>' % self.planeta_id
+
+    def serialize(self):
+        return {
+            "planeta_id": self.planeta_id,
+            "usuario_id": self.usuario_id
+        }
+
+class PersonajeFavoritos(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    __tablename__ = "personaje_favoritos"
+    usuario_id = db.Column(db.Integer,db.ForeignKey('usuario.id'))
+    personaje_id = db.Column(db.Integer, nullable=False)
+
+    def __init__ (self, usuario_id, personaje_id):
+        self.usuario_id = usuario_id
+        self.personaje_id = personaje_id
+
+    def __repr__(self):
+        return '<PersonajeFavoritos %r>' % self.personaje_id
+
+    def serialize(self):
+        return{
+            "personaje_id": self.personaje_id
         }
